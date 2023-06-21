a6418af45324f49170ef83b66751ceeacd81c5c4
arunsingh28 <arunsingh28aug.as@gmail.com>
2023-06-21 09:16:51 +0530

    update server

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,16 +7,18 @@ import database from '../config/database'
 dotenv.config()
 const app = express()
 
-
+// enable cors for accept requests
 app.use(cors())
+// parse json data
 app.use(express.json())
 
+// connect to database
 database
 
 app.use('/', mailRouter)
 app.use('/status/:id', mailStatus)
 
-
+// listen to port 4000
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
